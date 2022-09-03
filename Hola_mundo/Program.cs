f22dcc7225a1d6dea7664dfa99dbbc391144b705
IsaiasMCC <isaisvhsyw@gmail.com>
2022-09-03 01:02:12 -0400

    Hellow World triangle

---
+++ Hola_mundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Hola_mundo.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hola_mundo
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+            Game game = new Game(800, 600, "Hellow World");
+        }
+    }
+}
---
+++ Hola_mundo/models/Game.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hola_mundo.shaders;
+//openTK
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace Hola_mundo.models
+{
+    public class Game : GameWindow
+    {
+        private float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+             0.5f, -0.5f, 0.0f, //Bottom-right vertex
+             0.0f,  0.5f, 0.0f  //Top vertex
+        };
+        int VertexBufferObject; // id VertexBufferObject
+        int VertexArrayObject; //      VAO
+        
+        // shaders
+        Shader shader;
+
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
+        {
+            Run(60.0);
+        }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            KeyboardState input = Keyboard.GetState();
+
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+
+            base.OnUpdateFrame(e);
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            VertexBufferObject = GL.GenBuffer();
+            shader = new Shader("shader.vert", "shader.frag");
+            VertexArrayObject = GL.GenVertexArray();
+
+            // 1. bind Vertex Array Object
+            GL.BindVertexArray(VertexArrayObject);
+            // 2. copy our vertices array in a buffer for OpenGL to use
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            // 3. then set our vertex attributes pointers
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            base.OnLoad(e);
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code goes here.
+            shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VertexBufferObject);
+            shader.Dispose();
+            base.OnUnload(e);
+        }
+    }
+}
